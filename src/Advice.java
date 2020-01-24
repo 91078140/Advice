2b5569fa344edf99c47daa93683bf6dbb1e2ef6b
91078140 <91078140@mvusd.net>
2020-01-24 13:46:47 -0800

    Work copied from a textbook

---
+++ src/Advice.java
@@ -0,0 +1,10 @@
+//Timothy Wilks
+// child class
+public class Advice extends Thought
+{
+    public void message()
+    {
+        System.out.println("Think before you speak.");
+        super.message();
+    }
+}// end of advice class
---
+++ src/Thought.java
@@ -0,0 +1,8 @@
+//parent class
+public class Thought
+{
+    public void message()
+    {
+        System.out.println("I'm thinking, I'm thinking.");
+    }
+}//end of thought class
